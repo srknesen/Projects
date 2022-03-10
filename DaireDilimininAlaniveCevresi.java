173b3b4642ec3f7f323a598a4069f33ed557e066
srknesen <84016162+srknesen@users.noreply.github.com>
2022-03-10 11:36:46 +0300

    Update and rename DaireDilimininAlani.java to DaireDilimininAlaniveCevresi.java

--- DaireDilimininAlaniveCevresi.java
+++ DaireDilimininAlaniveCevresi.java
@@ -1,16 +1,19 @@
-//Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanını bulan program
+//Yarıçapı r, merkez açısının ölçüsü 𝛼 olan daire diliminin alanını ve çevresini bulan program
 import java.util.Scanner;
 
-public class DaireDilimininAlani {
+public class DaireDilimininAlaniOdev {
     public static void main(String[] args) {
-        double r, pi=3.14,aci,alan;
+        double r, pi=3.14,aci,alan,cevre;
         Scanner input= new Scanner(System.in);
         System.out.println("Dairienin Yarıçapını Giriniz: ");
         r= input.nextDouble();
         System.out.println("Daire Diliminin Açı Ölçüsünü Giriniz: ");
-        
         aci= input.nextDouble();
+        
         alan=(pi*(r*r)*aci)/360;
+        cevre=(2*pi*r*aci)/360;
+        
         System.out.println("Daire Diliminin Alanı: "+alan);
+        System.out.println("Daire Diliminin Çevresi: "+cevre);
     }
 }
